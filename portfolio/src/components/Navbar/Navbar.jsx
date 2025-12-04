afc86a3746110d2daa21e97d5f575da3cf26d809
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2025-12-04 18:55:47 +0100

    navbar

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./Navbar.module.css";
+import {getImageUrl} from "../../utils.js";
 
 export default function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <nav className={styles.navbar}>
+    <nav className={styles.navbarBg}>
+    <div className={styles.navbar}>
       <a className={styles.title} href="/">Rocio Arenillas</a>
       <div className={styles.menu}>
-        <ul className={styles.menuItem}>
+        <img className={styles.menuBtn} 
+        src={menuOpen 
+            ? getImageUrl("nav/closeIcon.png")
+            : getImageUrl("nav/menuIcon.png")
+        } 
+        alt="menu-button"
+        onClick={() => setMenuOpen(!menuOpen)} />
+        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+        onClick={() => setMenuOpen(false)}>
             <li href="#about">About</li>
             <li href="#about">Skills & Tools</li>
             <li href="#about">Projects</li>
             <li href="#about">Contact</li>
         </ul>
+      </div>
       </div>
     </nav>
   )
