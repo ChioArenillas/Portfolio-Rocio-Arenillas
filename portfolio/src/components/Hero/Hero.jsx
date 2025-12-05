5e6d6c90f75fb1311755201426f928a36e8b889c
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2025-12-05 11:02:18 +0100

    new

---
+++ portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { getImageUrl } from '../../utils'
+
+export default function Hero() {
+  return (
+    <div>
+      <img src="C:\Users\raren\Desktop\Portfolio Rocio Arenillas\portfolio\assets\hero\top-picture.jpg" ></img>
+    </div>
+  )
+}
--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import styles from "./Navbar.module.css";
-import {getImageUrl} from "../../utils.js";
+import { getImageUrl } from "../../utils.js";
 
 
 export default function Navbar() {
@@ -9,7 +9,7 @@ export default function Navbar() {
   return (
     <nav className={styles.navbarBg}>
     <div className={styles.navbar}>
-      <a className={styles.title} href="/">Rocio Arenillas</a>
+      <a className={styles.title} href="/">&lt; Rocio Arenillas /&gt;</a>
       <div className={styles.menu}>
         <img className={styles.menuBtn} 
         src={menuOpen 
@@ -21,12 +21,12 @@ export default function Navbar() {
         <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
         onClick={() => setMenuOpen(false)}>
             <li href="#about">About</li>
-            <li href="#about">Skills & Tools</li>
+            <li href="#about">Skills&Tools</li>
             <li href="#about">Projects</li>
             <li href="#about">Contact</li>
         </ul>
       </div>
       </div>
     </nav>
-  )
-}
+  );
+};
