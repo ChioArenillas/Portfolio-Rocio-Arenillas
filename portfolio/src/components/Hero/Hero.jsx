5f1e5aecca0a6b494ebaa3035ede59403a36a912
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2025-12-09 18:43:44 +0100

    new

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -7,16 +7,21 @@ export default function Hero() {
     <div className={styles.hero} >
       <img className={styles.heroimg} src={topPicture} alt="hero" />
       <div className={styles.greyBackground}></div>
-        <div className={styles.text}>  
-            <div className={styles.textLeft}>
-                <h2 className={styles.title}>DESIGN</h2>
-                <p className={styles.subtitle}>UX UI Designer</p>
-            </div>
-            <div className={styles.textRight}>
-                <h2 className={styles.title}>&lt;CODE/&gt;</h2>
-              <p className={styles.subtitle}>Frontend Developer</p>
-            </div>          
-        </div>      
+      <div className={styles.text}>
+        <div className={styles.textLeft}>
+          <h2 className={styles.title}>DESIGN</h2>
+          <p className={styles.subtitle}>
+            UX UI <span className={styles.subtitleSecond}>Designer</span>
+          </p>
+        </div>
+
+        <div className={styles.textRight}>
+          <h2 className={styles.title}>&lt;CODE/&gt;</h2>
+          <p className={styles.subtitle}>
+            Frontend <span className={styles.subtitleSecond}>Developer</span>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
