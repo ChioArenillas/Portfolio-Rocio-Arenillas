ac0ac9b380c54bb42d22bb3d628dce3bda8b1797
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2025-12-09 18:32:02 +0100

    new

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -10,11 +10,11 @@ export default function Hero() {
         <div className={styles.text}>  
             <div className={styles.textLeft}>
                 <h2 className={styles.title}>DESIGN</h2>
-                <p className={styles.subtitle}>UX UI designer</p>
+                <p className={styles.subtitle}>UX UI Designer</p>
             </div>
             <div className={styles.textRight}>
                 <h2 className={styles.title}>&lt;CODE/&gt;</h2>
-              <p className={styles.subtitle}>Frontend developer</p>
+              <p className={styles.subtitle}>Frontend Developer</p>
             </div>          
         </div>      
     </div>
